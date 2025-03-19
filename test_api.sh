8564ee001eb9e73692ceec2c3cec81eb556a22a6
manjumh021 <81350837+manjumh021@users.noreply.github.com>
2025-03-19 19:11:17 +0530

    Update and rename test-api.sh to test_api.sh

--- test_api.sh
+++ test_api.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Start the Flask application in a Docker container
-docker run -d -p 5000:5000 --name flask-app my-flask-app
-
-# Wait for the application to start
-sleep 5
-
 # Send a request to the /add endpoint
 response=$(curl -s "http://localhost:5000/add?num1=5&num2=3")
 
@@ -17,7 +11,3 @@ else
   echo "Test failed: The response is $response, expected 8."
   exit 1
 fi
-
-# Clean up
-docker stop flask-app
-docker rm flask-app
